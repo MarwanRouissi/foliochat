31d81845c4158ba78462951bf3c7809c6576a33a
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-06-04 10:44:39 +0200

    added fields to project model

--- db/migrate/20250604082614_create_projects.rb
+++ db/migrate/20250604082614_create_projects.rb
@@ -1,6 +1,14 @@
 class CreateProjects < ActiveRecord::Migration[7.2]
   def change
     create_table :projects do |t|
+      t.string :name
+      t.string :description
+      t.string :github_url
+      t.text :technologies, array: true
+      t.text :features, array: true
+      t.text :challenges, array: true
+      t.text :potential_improvements, array: true
+
       t.timestamps
     end
   end
