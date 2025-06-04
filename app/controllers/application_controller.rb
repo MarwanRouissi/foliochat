dcba52e63d9e141a621aeadb88ddf7afe542e726
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-06-04 12:10:57 +0200

    added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
